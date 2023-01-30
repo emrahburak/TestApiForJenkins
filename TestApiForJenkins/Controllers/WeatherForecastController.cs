920d8a81adada391ebbe9dcced577b8f1cc1637a
emrahburak <gemrahburak@gmail.com>
2023-01-30 13:58:36 +0300

    added yağmurlu

--- TestApiForJenkins/Controllers/WeatherForecastController.cs
+++ TestApiForJenkins/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace TestApiForJenkins.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching" ,"Yağmurlu"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
